2221bd18841687e33e01be7f1e8dde41055b8e24
Nicolas Fiquitiva Segura <Nico52105@gmail.com>
2024-11-16 17:40:04 -0500

    Se crean enpoints de cliente

---
+++ ApiColmenaSeguros/Controllers/ClienteController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiColmenaSeguros.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClienteController : ControllerBase
+    {
+        IRespuestas iRespuestas;
+        public ClienteController(IRespuestas irespuestas)
+        {
+            this.iRespuestas = irespuestas;
+        }
+
+        [HttpGet("/[Action]")]
+        public ActionResult<string> CrearCuenta()
+        {
+            return iRespuestas.Formato("Operacion exitosa");            
+        }
+
+        [HttpGet("/[Action]")]
+        public ActionResult<string> ActualizarCuenta()
+        {
+            return iRespuestas.Formato("Operacion exitosa");
+        }
+
+        [HttpGet("/[Action]")]
+        public ActionResult<string> IniciarSesion()
+        {
+            return iRespuestas.Formato("Operacion exitosa");
+        }
+
+        [HttpGet("/[Action]")]
+        public ActionResult<string> RecuperarContraseña()
+        {
+            return iRespuestas.Formato("Operacion exitosa");
+        }        
+
+        [HttpGet("/[Action]")]
+        public ActionResult<string> MisCotizaciones()
+        {
+            return iRespuestas.Formato("Operacion exitosa");
+        }
+
+        [HttpGet("/[Action]")]
+        public ActionResult<string> MisPolizas()
+        {
+            return iRespuestas.Formato("Operacion exitosa");
+        }
+
+        [HttpGet("/[Action]")]
+        public ActionResult<string> MisCoberturas()
+        {
+            return iRespuestas.Formato("Operacion exitosa");
+        }
+    }
+}
---
+++ ApiColmenaSeguros/Respuestas.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiColmenaSeguros
+{
+
+    public interface IRespuestas
+    {
+        public ActionResult<string> Formato(string data);
+    }
+    public class Respuestas : ControllerBase,IRespuestas
+    {
+        public ActionResult<string> Formato(string data)
+        {
+            return Ok(data);
+        }
+    }
+}
